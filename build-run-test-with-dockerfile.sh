eaeda888929a661646bf230053ad25dae6c22642
Nicolas Auricchio <aurinicolas@hotmail.com>
2017-11-06 15:44:00 +0100

    scripts modified

---
+++ build-run-test-with-dockerfile.sh
@@ -0,0 +1,8 @@
+docker rm -f hello-world > /dev/null
+docker rmi localhost:5000/hello-world:latest > /dev/null
+
+docker build -f Dockerfile -t localhost:5000/hello-world .
+docker run -d --name hello-world -p 8080:8080 localhost:5000/hello-world:latest
+
+sleep 10
+curl localhost:8080

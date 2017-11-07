7513660951e14586f167d681aa64777e617f8ee3
Nicolas Auricchio <aurinicolas@hotmail.com>
2017-11-07 10:27:08 +0100

    Modified scripts

--- create-network-and-add-container.sh
+++ create-network-and-add-container.sh
@@ -1,4 +1,6 @@
 docker rm -f hello-world
+
+docker network create demo
 docker run -d --name hello-world  --network demo --hostname hello-world-application localhost:5000/hello-world:latest
 
 docker network inspect demo
